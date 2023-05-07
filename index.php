352d021fa940b0d0563b2ea6a93384838f7320cb
Patrick Daxboeck <39397393+daxboeck@users.noreply.github.com>
2023-05-07 18:45:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,65 +1,37 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-
 session_start();
 
-$container = new \Slim\Container();
-$container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__ . '/views', [
-        'cache' => false,
-    ]);
+require_once 'vendor/autoload.php';
+use App\Controllers\AuthController;
 
-    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+$authController = new AuthController();
 
-    return $view;
-};
+// If user is already logged in, redirect to dashboard
+if (isset($_SESSION['user_id'])) {
+    header('Location: dashboard.php');
+    exit();
+}
 
-$app = new \Slim\App($container);
-
-$authMiddleware = function ($request, $response, $next) {
-    if (!isset($_SESSION['user'])) {
-        return $response->withRedirect('/login');
-    }
-
-    return $next($request, $response);
-};
-
-$loginMiddleware = function ($request, $response, $next) {
-    if (isset($_SESSION['user'])) {
-        return $response->withRedirect('/dashboard');
-    }
-
-    return $next($request, $response);
-};
-
-$app->get('/', function ($request, $response, $args) {
-    return $this->view->render($response, 'index.twig');
-})->setName('index');
-
-$app->group('', function () use ($app) {
-    $app->get('/login', function ($request, $response, $args) {
-        return $this->view->render($response, 'login.twig');
-    })->add($loginMiddleware)->setName('login');
-
-    $app->post('/login', 'AuthController:login')->add($loginMiddleware);
-
-    $app->get('/logout', 'AuthController:logout');
-});
-
-$app->group('', function () use ($app, $authMiddleware) {
-    $app->get('/dashboard', 'LanguageController:dashboard')->add($authMiddleware)->setName('dashboard');
-
-    $app->get('/train', 'LanguageController:train')->add($authMiddleware)->setName('train');
-
-    $app->get('/edit/{id}', 'LanguageController:edit')->add($authMiddleware)->setName('edit');
-    $app->post('/edit/{id}', 'LanguageController:update')->add($authMiddleware);
-
-    $app->get('/add', 'LanguageController:add')->add($authMiddleware)->setName('add');
-    $app->post('/add', 'LanguageController:store')->add($authMiddleware);
-
-    $app->get('/remove/{id}', 'LanguageController:remove')->add($authMiddleware)->setName('remove');
-
-})->add($authMiddleware);
-
-$app->run();
+// If form is submitted, try to log in the user
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+    $authController->login($_POST);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Login</h1>
+        <form method="post">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="submit" name="submit" value="Log in">
+        </form>
+    </div>
+</body>
+</html>
